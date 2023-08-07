5a7973d1b6ccda0d28f7cf4b421b1b064a17557a
slystersanto <santhosh.mech.19@gmail.com>
2023-08-07 13:44:09 +0530

    new changes for deploy

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://effervescent-tulumba-cfa789.netlify.app',
   credentials: true
 }));
 
